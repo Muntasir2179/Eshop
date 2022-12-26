65f449618c453fedcc0a9ceea8ae2f7e10cf018b
Muntaisr Ahmed <muntasirahmed2179@gmail.com>
2022-12-26 15:50:50 +0600

    Initial Commit (Models Created)

---
+++ store/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Product, Category
+
+
+class AdminProduct(admin.ModelAdmin):
+    # setting the table columns name to show in the admin site
+    list_display = ['name', 'price', 'category']
+
+
+class AdminCategory(admin.ModelAdmin):
+    # setting the table columns name to show in the admin site
+    list_display = ['name']
+
+
+# Register your models here.
+admin.site.register(Product, AdminProduct)
+admin.site.register(Category, AdminCategory)
