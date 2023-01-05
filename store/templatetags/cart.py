8f51c8dbfc0a15e8569b462111c065ccb0b09623
Muntaisr Ahmed <muntasirahmed2179@gmail.com>
2023-01-05 23:23:33 +0600

    Cart table complete

--- store/templatetags/cart.py
+++ store/templatetags/cart.py
@@ -22,3 +22,22 @@ def cart_quantity(product, cart):
         if int(id) == product.id:
             return cart.get(id)
     return 0
+
+
+@register.filter(name='price_total')
+def price_total(product, cart):
+    return product.price * cart_quantity(product, cart)
+
+
+@register.filter(name='total_cart_price')
+def total_cart_price(products, cart):
+    sum = 0
+    for p in products:
+        sum += price_total(p, cart)
+    return sum
+
+
+# filter to concat taka symbol with amount
+@register.filter(name='currency')
+def currency(number):
+    return "৳" + str(number)
